70c0394b02d034b4aacfdee0e932abedd9f5a0b9
Matheus Teixeira <hardway3@protonmail.com>
2021-02-13 15:59:15 -0300

    feat: implemented requests to api storm glass

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -1,20 +1,6 @@
 import { ForecastPoint, StormGlass } from '@src/clients/stormGlass';
+import { Beach } from '@src/model/beach';
 import { InternalError } from '@src/util/erros/internal-error';
-
-export enum BeachPosition {
-  S = 'S',
-  E = 'E',
-  N = 'N',
-  W = 'W'
-}
-
-export interface Beach {
-  name: string,
-  position: BeachPosition,
-  lat: number,
-  lng: number,
-  user: string,
-}
 
 export interface TimeForecast {
   time: string;
