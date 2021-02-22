0f523383bee39b156cbf7d2bf6dd592cabf16786
matthaw <hardway3@protonmail.com>
2021-02-22 17:26:55 -0300

    feat: Token Attention Middleware

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,19 +1,25 @@
-import { Controller, Post } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Post } from '@overnightjs/core';
+import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/model/beach';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import { BaseController } from '.';
 
 @Controller('beaches')
+@ClassMiddleware(authMiddleware)
 export class BeachesController extends BaseController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
     try {
-      const beach = new Beach(req.body);
+      const beach = new Beach({ ...req.body, ...{ user: req.decoded?.id } });
       const result = await beach.save();
       res.status(201).send(result);
     } catch (error) {
-      this.sendCreateUpdateErrorResponse(res, error);
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: error.message });
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
     }
   }
 }
