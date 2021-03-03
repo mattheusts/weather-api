e5c9546692aca37f144234045a3b809b63843160
matthaw <hardway3@protonmail.com>
2021-03-03 18:03:00 -0300

    feat: route for information about me

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Get, Middleware, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/model/user';
 import { BaseController } from '.';
 import AuthService from '@src/services/auth';
 import ApiError from '@src/util/erros/api-error';
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -32,5 +33,18 @@ export class UsersController extends BaseController {
 
     const token = AuthService.generateToken(user.toJSON());
     return res.status(200).send({ token: token });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me(req: Request, res: Response): Promise<Response> {
+    const email = req.decoded ? req.decoded?.email : undefined;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return this.sendErrorResponse(res, { code: 404, message: 'User not found!' });
+    }
+
+    return res.send({ user });
   }
 }
