a739b30a577465dad1eb80700a35996cf08cfab2
matthaw <hardway3@protonmail.com>
2021-03-11 17:40:56 -0300

    test: request refactoring errors

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -19,7 +19,7 @@ describe('users functional tests', () => {
       expect(response.body).toEqual(expect.objectContaining({ ...newUser, ...{ password: expect.any(String) }, }));
     });
 
-    it('Should return 422 when there is a validation error', async () => {
+    it('Should return a validation error when field is missing', async () => {
       const newUser = {
         email: 'john@mail.com',
         password: '1234',
@@ -27,10 +27,10 @@ describe('users functional tests', () => {
 
       const response = await global.testRequest.post('/users').send(newUser);
 
-      expect(response.status).toBe(422);
+      expect(response.status).toBe(400);
       expect(response.body).toEqual({
-        code: 422,
-        error: 'Unprocessable Entity',
+        code: 400,
+        error: 'Bad Request',
         message: 'User validation failed: name: Path `name` is required.',
       });
     });
